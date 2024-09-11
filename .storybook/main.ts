8f5b3ad4fa4554e0aa51133e62956fd76bd9de93
Austin Isinghood <austin@austinisinghood.com>
2024-09-10 18:02:40 -0600

    Upgrading Storybook.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,17 +4,28 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   // Required
   framework: '@storybook/react-vite',
+
   stories: [
     '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     '../storybook/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+
   // Optional
-  addons: ['@storybook/addon-essentials'],
-  docs: {
-    autodocs: 'tag',
-  },
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-mdx-gfm',
+    '@chromatic-com/storybook'
+  ],
+
+  docs: {},
+
   staticDirs: ['../public'],
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 
 export default config
